2596c6f6fa3831d971bded9a3c4d2d0df4fe38d3
Abulfida Akmal <echo.ablfda@gmail.com>
2024-11-12 19:09:12 +0800

    add sonner package and customize sonner toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
+import Toaster from "./components/Toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <BrowserRouter>
+        <Toaster />
         <App />
       </BrowserRouter>
     </ClerkProvider>
