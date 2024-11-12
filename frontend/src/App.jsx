4ac43e780f93b5942226d8aa9caa00fe317144b3
Abulfida Akmal <echo.ablfda@gmail.com>
2024-11-12 16:24:22 +0800

    add sign-in page and sign-up page with clerk

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Hello World</h1>} />
+      <Route path="/sign-in" element={<SignInPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
     </Routes>
   );
 }
