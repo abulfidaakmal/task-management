344f3f1748d06de2cdd2d2ac11629a9b4f355eee
Abulfida Akmal <echo.ablfda@gmail.com>
2024-11-13 10:38:54 +0800

    add loading screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 import Toaster from "./components/Toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <Toaster />
-        <App />
-      </BrowserRouter>
+      <ClerkLoading>
+        <div className="flex justify-center h-screen">
+          <span className="loading loading-spinner loading-lg" />
+        </div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <BrowserRouter>
+          <Toaster />
+          <App />
+        </BrowserRouter>
+      </ClerkLoaded>
     </ClerkProvider>
   </StrictMode>
 );
